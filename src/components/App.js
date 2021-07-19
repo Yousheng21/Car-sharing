a7317ed03c6969834bea6baad5ae36d1ae80149a
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-07-20 00:43:13 +0300

    updating

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,21 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import React from "react";
-// eslint-disable-next-line import/extensions
-import Home from "./Home/Home.jsx";
+import Home from "./Home/Home";
+import Model from "./Order/Model/Model";
+import Extra from "./Order/Extra/Extra";
+import Total from "./Order/Total/Total";
+import Location from "./Order/Location/Location";
 
 function App() {
   return (
     // eslint-disable-next-line react/jsx-filename-extension
     <BrowserRouter>
       <Switch>
-        <Route path="/car-sharing" component={Home} />
+        <Route exact path="/car-sharing/home" component={Home} />
+        <Route exact path="/car-sharing/order" component={Location} />
+        <Route exact path="/car-sharing/order/model" component={Model} />
+        <Route exact path="/car-sharing/order/extra" component={Extra} />
+        <Route exact path="/car-sharing/order/total" component={Total} />
       </Switch>
     </BrowserRouter>
   );
---
+++ src/utils/tempates/SideBar/SideBar.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import "./sideBar.css";
+import { useSelector } from "react-redux";
+
+// eslint-disable-next-line react/prop-types
+const SideBar = ({ children }) => {
+  const parameters = useSelector((state) => state.app.currentOrder);
+  const price = useSelector((state) => state.app.price);
+
+  return (
+    <div className="sideBar">
+      <div className="sideBar-info-title">
+        <h1>Ваш заказ:</h1>
+      </div>
+      <div className="sideBar-parameters">
+        {Object.keys(parameters).map((item, index) => {
+          return parameters[item].value ? (
+            // eslint-disable-next-line react/no-array-index-key
+            <div key={index} className="sideBar-info-content">
+              <span className="place">{parameters[item].text}</span>
+              <span className="dots">......................</span>
+              <div className="address">
+                <span>{parameters[item].value}</span>
+              </div>
+            </div>
+          ) : (
+            ""
+          );
+        })}
+      </div>
+
+      <div className="sideBar-info-price">
+        <h1>
+          Цена от <span>{price.min}</span> до <span>{price.max}</span>
+        </h1>
+      </div>
+      {children}
+    </div>
+  );
+};
+
+export default SideBar;
