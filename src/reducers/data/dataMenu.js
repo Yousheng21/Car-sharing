21f952cc702b8352102ea7170039379292e2c82f
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-07-20 21:11:12 +0300

    информация в меню

---
+++ src/reducers/data/dataMenu.js
@@ -0,0 +1,14 @@
+import telegram from "../../images/telegram.png";
+import facebook from "../../images/facebook.png";
+import instagram from "../../images/instagram.png";
+
+const components = {
+  li: ["ПАРКОВКА", "СТРАХОВКА", "БЕНЗИН", "ОБСЛУЖИВАНИЕ"],
+  socLink: [
+    { text: "telegram", img: telegram },
+    { text: "facebook", img: facebook },
+    { text: "instagram", img: instagram },
+  ],
+};
+
+export default components;
