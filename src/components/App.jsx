1f9edd69adfa52bb8499040506d9c8b430c202bc
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-07-20 20:44:39 +0300

    удаление exact в ненужных роутах

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,14 +8,13 @@ import Location from "./Order/Location/Location";
 
 function App() {
   return (
-    // eslint-disable-next-line react/jsx-filename-extension
     <BrowserRouter>
       <Switch>
-        <Route exact path="/car-sharing/home" component={Home} />
+        <Route path="/car-sharing/home" component={Home} />
         <Route exact path="/car-sharing/order" component={Location} />
-        <Route exact path="/car-sharing/order/model" component={Model} />
-        <Route exact path="/car-sharing/order/extra" component={Extra} />
-        <Route exact path="/car-sharing/order/total" component={Total} />
+        <Route path="/car-sharing/order/model" component={Model} />
+        <Route path="/car-sharing/order/extra" component={Extra} />
+        <Route path="/car-sharing/order/total" component={Total} />
       </Switch>
     </BrowserRouter>
   );
