d1bc11eb30c603947634ab9417387d456dbbbd08
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-07-20 20:42:45 +0300

    включение линта

--- src/index.jsx
+++ src/index.jsx
@@ -7,7 +7,6 @@ import App from "./components/App";
 import { store } from "./reducers";
 
 ReactDOM.render(
-  // eslint-disable-next-line react/jsx-filename-extension
   <Provider store={store}>
     <App />
   </Provider>,
