38a51f9899c5503e50c4dee151a2b9da8d75b0c4
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-07-21 23:24:34 +0300

    Переименование файла стилей

--- src/components/Order/Location/Location.jsx
+++ src/components/Order/Location/Location.jsx
@@ -4,7 +4,7 @@ import NavBar from "../../common/NavBar/NavBar";
 import SideBar from "../../common/SideBar/SideBar";
 
 import map from "../../../images/map.png";
-import "./location.css";
+import "./location.scss";
 
 const Location = () => {
   return (
