75d137d7efb24744cd2f88a348595b9bfa73f2e4
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-07-21 23:25:15 +0300

    Переименование файла стилей

--- src/components/common/NavBar/NavBar.jsx
+++ src/components/common/NavBar/NavBar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Vector from "../../../images/vector.svg";
-import "./navBar.css";
+import "./navBar.scss";
 
 const NavBar = () => {
   return (
