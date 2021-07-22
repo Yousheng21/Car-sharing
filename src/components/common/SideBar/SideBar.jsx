9c446e9751353f73a36e7cf64b87c0c86f15800c
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-07-22 18:16:36 +0300

    Применение семантических тегов

--- src/components/common/SideBar/SideBar.jsx
+++ src/components/common/SideBar/SideBar.jsx
@@ -2,39 +2,41 @@ import React from "react";
 import "./sideBar.scss";
 import { useSelector } from "react-redux";
 
-// eslint-disable-next-line react/prop-types
 const SideBar = ({ children }) => {
   const parameters = useSelector((state) => state.app.currentOrder);
   const price = useSelector((state) => state.app.price);
 
   return (
-    <div className="sideBar">
-      <div className="sideBar-info-title">
+    <aside className="sideBar">
+      <header className="sideBar-info-title sideBar-child">
         <h1>Ваш заказ:</h1>
-      </div>
-      <div className="sideBar-parameters">
+      </header>
+      <main className="sideBar-parameters sideBar-child">
         {Object.keys(parameters).map((item) => {
           return (
             parameters[item].value && (
-              <div key={parameters[item].text} className="sideBar-info-content">
+              <section
+                key={parameters[item].text}
+                className="sideBar-info-content"
+              >
                 <span className="place">{parameters[item].text}</span>
                 <span className="dots">......................</span>
                 <div className="address">
                   <span>{parameters[item].value}</span>
                 </div>
-              </div>
+              </section>
             )
           );
         })}
-      </div>
+      </main>
 
-      <div className="sideBar-info-price">
-        <h1>
+      <footer className="sideBar-info-price sideBar-child">
+        <text>
           Цена от <span>{price.min}</span> до <span>{price.max}</span>
-        </h1>
-      </div>
+        </text>
+      </footer>
       {children}
-    </div>
+    </aside>
   );
 };
 
