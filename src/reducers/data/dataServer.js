173eaf9ba33e2c1e5740dc8e04db3b4168164e75
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-07-24 13:47:07 +0300

    данные для запроса

---
+++ src/reducers/data/dataServer.js
@@ -0,0 +1,6 @@
+export const API_URL = "https://api-factory.simbirsoft1.com";
+
+export const headers = {
+  "X-Api-Factory-Application-Id": "5e25c641099b810b946c5d5b",
+  Authorization: "Bearer 52efbe08228671240494f537f2486bc109a637b4",
+};
