c04ec43f7ef1b725f73228fdd97a486844880472
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-07-24 13:30:44 +0300

    реализация экшена

--- src/actions/app.js
+++ src/actions/app.js
@@ -1,5 +1,38 @@
 import axios from "axios";
-import saveToLocalStorage, {store} from "../reducers";
+import { store } from "../reducers";
 
+import { API_URL, headers } from "../reducers/data/dataServer";
+import { setTableCars, setNewTableCars } from "../reducers/appReducer";
 
+const getTableCars = () => {
+  return async (dispatch) => {
+    try {
+      const response = await axios.get(
+        `${API_URL}/api/db/car?limit=6&offset=2`,
+        {
+          headers: {
+            "X-Api-Factory-Application-Id":
+              headers["X-Api-Factory-Application-Id"],
+            Authorization: headers.Authorization,
+          },
+        }
+      );
+      dispatch(setTableCars(response.data.data));
+    } catch (e) {
+      console.log(e.response);
+    }
+  };
+};
 
+export const selectSortCars = (sort) => {
+  const cars = store.getState().app.tableCars;
+  const newCars = !sort
+    ? cars
+    : cars.filter((item) => {
+        return item.categoryId.name === sort;
+      });
+
+  store.dispatch(setNewTableCars(newCars));
+};
+
+export default getTableCars();
