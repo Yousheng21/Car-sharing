0a2db2f855f0c8fa2626e55aac4d3de3555513fc
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-07-30 01:34:50 +0300

    добавление информации об api

--- src/reducers/data/dataServer.js
+++ src/reducers/data/dataServer.js
@@ -1,6 +1,8 @@
 export const API_URL = "https://api-factory.simbirsoft1.com";
+export const API_GEO = "https://geocode-maps.yandex.ru/1.x";
+export const API_GEO_KEY = "ccc07d30-f6af-44a9-9d1f-875ed33ea03d";
 
-export const headers = {
+export const headersMain = {
   "X-Api-Factory-Application-Id": "5e25c641099b810b946c5d5b",
   Authorization: "Bearer 52efbe08228671240494f537f2486bc109a637b4",
 };
