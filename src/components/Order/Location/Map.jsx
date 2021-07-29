f22c11d618d8bc4b6d9b5189e34c3427d56b610f
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-07-30 01:06:17 +0300

    объект яндекс карт ( задание F3)

---
+++ src/components/Order/Location/Map.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+// import { Placemark, YMaps } from "react-yandex-maps";
+
+const Map = () => {
+  return (
+    <div />
+    // <YMaps>
+    //   <Map
+    //     defaultState={{
+    //       center: [55.75, 37.57],
+    //       zoom: 9,
+    //     }}
+    //     width="100%"
+    //     height="40vh"
+    //   >
+    //     <Placemark
+    //       options={{
+    //         preset: "islands#circleDotIcon",
+    //         iconColor: "#ff0000",
+    //       }}
+    //       properties={{
+    //         iconCaption: true,
+    //       }}
+    //       geometry={[55.75, 37.57]}
+    //     />
+    //   </Map>
+    // </YMaps>
+  );
+};
+
+export default Map;
