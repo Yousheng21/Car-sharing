3d212de5140818b3b98be49d9fd47a181ec6aa41
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-07-30 00:58:03 +0300

    экш получения геолокации адресов по апи

---
+++ src/actions/geolocate.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { API_GEO, API_GEO_KEY } from "../reducers/data/dataServer";
+import { setPlaceMarks } from "../reducers/appReducer";
+import { store } from "../reducers";
+
+const getLocale = (query) => {
+  return async (dispatch) => {
+    try {
+      const response = await axios.get(
+        `${API_GEO}/?format=json&apikey=${API_GEO_KEY}&geocode=${query}`
+      );
+      dispatch(
+        setPlaceMarks(
+          response.data.response.GeoObjectCollection.featureMember[0].GeoObject
+        )
+      );
+    } catch (e) {
+      console.log(e.response);
+    }
+  };
+};
+
+export const updateAddresses = (points) => {
+  return points.map((item) => {
+    return store.dispatch(getLocale(`${item.cityId.name} ${item.address}`));
+  });
+};
+
+export default getLocale();
