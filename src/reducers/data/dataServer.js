9c1c4dc5d2f3409a9f26742e9f74180cd8688b9b
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-07-31 13:12:18 +0300

    Создание instance и запись ключей в переменные

--- src/reducers/data/dataServer.js
+++ src/reducers/data/dataServer.js
@@ -1,8 +1,16 @@
+import axios from "axios";
+
+const APPLICATION_ID = "5e25c641099b810b946c5d5b";
+const AUTHORIZATION_API = "52efbe08228671240494f537f2486bc109a637b4";
+
 export const API_URL = "https://api-factory.simbirsoft1.com";
 export const API_GEO = "https://geocode-maps.yandex.ru/1.x";
 export const API_GEO_KEY = "ccc07d30-f6af-44a9-9d1f-875ed33ea03d";
 
-export const headersMain = {
-  "X-Api-Factory-Application-Id": "5e25c641099b810b946c5d5b",
-  Authorization: "Bearer 52efbe08228671240494f537f2486bc109a637b4",
-};
+export const instance = axios.create({
+  baseURL: "https://api-factory.simbirsoft1.com",
+  headers: {
+    "X-Api-Factory-Application-Id": APPLICATION_ID,
+    Authorization: `Bearer ${AUTHORIZATION_API}`,
+  },
+});
