9763fcdda18c1771f81f6a41e64793594adbf5ed
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-07-31 17:38:24 +0300

    V Создание лэйаута

---
+++ src/components/layouts/OrderLayout/OrderLayout.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import BurgerMenu from "../../common/Burger-menu/Burger-menu";
+import NavBar from "../../common/NavBar/NavBar";
+import SideBar from "../../common/SideBar/SideBar";
+import { nextStep } from "../../../actions/app";
+import "./orderLayout.scss";
+
+const OrderLayout = ({ children, path, step, text, page }) => {
+  return (
+    <aside className="location-page">
+      <BurgerMenu />
+      <NavBar page={page} />
+      {children}
+      <SideBar>
+        <Link to={`/car-sharing/order/${path}`}>
+          <button
+            type="button"
+            onClick={() => nextStep(step)}
+            className="sideBar-button sideBar-child"
+          >
+            {text}
+          </button>
+        </Link>
+      </SideBar>
+    </aside>
+  );
+};
+
+export default OrderLayout;
