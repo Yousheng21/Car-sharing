4d4c656a354a183a7791099e8caffaf3029f0c77
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-08-02 02:08:13 +0300

    F-03 Разделение кода

--- src/actions/address.js
+++ src/actions/address.js
@@ -1,7 +1,6 @@
 import { store } from "../reducers";
 import { instance } from "../reducers/data/dataServer";
 import { setAddresses, setNewAddresses } from "../reducers/appReducer";
-import { updateAddresses } from "./geolocate";
 
 const LIMIT_VALUE = 10;
 
@@ -16,7 +15,6 @@ export const getTableAddress = (cityId) => {
         },
       });
       dispatch(setAddresses(response.data.data));
-      updateAddresses(response.data.data);
     } catch (e) {
       console.error(e.response);
     }
@@ -27,22 +25,17 @@ export const searchAddress = (query) => {
   const { addresses } = store.getState().app;
   const city = store.getState().app.currentCity;
   const regExp = new RegExp(query.toLowerCase());
-  let element;
 
-  function success(item) {
-    element = item;
-    if ("name" in city) {
-      if (city.name === item.cityId.name) element = item;
-    }
-    return regExp.test(element.address.toLowerCase());
+  function hasCity(item) {
+    if (city.name === item.cityId.name)
+      return regExp.test(item.address.toLowerCase());
   }
-  return store.dispatch(setNewAddresses(addresses.filter(success)));
-};
 
-export const selectAddresses = (city) => {
-  const { addresses } = store.getState().app;
-  const newAddresses = addresses.filter((item) => {
-    return item.cityId.name === city;
-  });
-  return store.dispatch(setNewAddresses(newAddresses));
+  function withoutCity(item) {
+    return regExp.test(item.address.toLowerCase());
+  }
+
+  return store.dispatch(
+    setNewAddresses(addresses.filter("name" in city ? hasCity : withoutCity))
+  );
 };
