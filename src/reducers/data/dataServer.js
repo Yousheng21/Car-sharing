07141dab3400f54f1e84d3363dcb056002b9cd03
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-08-03 23:07:37 +0300

    F-03 Замена http на https

--- src/reducers/data/dataServer.js
+++ src/reducers/data/dataServer.js
@@ -4,7 +4,7 @@ const APPLICATION_ID = "5e25c641099b810b946c5d5b";
 const AUTHORIZATION_API = "52efbe08228671240494f537f2486bc109a637b4";
 
 export const API_URL = "https://api-factory.simbirsoft1.com";
-export const API_GEO = "http://nominatim.openstreetmap.org/search";
+export const API_GEO = "https://nominatim.openstreetmap.org/search";
 export const API_GEO_KEY = "s9CEluxAEtMl";
 
 export const instance = axios.create({
