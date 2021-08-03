b5b7a42253835d65ced86745c012e89198ad510b
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-08-03 21:33:43 +0300

    F-03 Реализация карты leaflet c переносом к опр пункту

--- src/components/Order/Location/Map.jsx
+++ src/components/Order/Location/Map.jsx
@@ -1,32 +1,67 @@
-import React from "react";
-// import { Placemark, YMaps } from "react-yandex-maps";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import L from "leaflet";
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
+import markerIconPng from "leaflet/dist/images/marker-icon.png";
+import "leaflet/dist/leaflet.css";
 
-/* Заккомитил, так как сайт стоит из за яндекс карт, но в дальнейшем они будут нужны */
+const customMarker = new L.icon({
+  iconUrl: markerIconPng,
+  iconSize: [25, 41],
+  iconAnchor: [10, 41],
+  popupAnchor: [2, -40],
+});
 
 const Map = () => {
+  const marks = useSelector((state) => state.app.placeMarks);
+  const currMarkIndex = useSelector((state) => state.app.placeMarkIndex);
+  const [center, setCenter] = useState([54.1838, 45.1749]);
+
+  useEffect(() => {
+    if (marks.length) setCenter([Number(marks[0].lat), Number(marks[0].lon)]);
+  }, []);
+
+  function MyComponent() {
+    const map = useMap();
+    useEffect(() => {
+      if (Object.keys(currMarkIndex).length) {
+        map.flyTo(
+          [Number(currMarkIndex.lat), Number(currMarkIndex.lon)],
+          map.getZoom()
+        );
+      }
+    }, [currMarkIndex]);
+
+    return null;
+  }
+
   return (
-    <div />
-    // <YMaps>
-    //   <Map
-    //     defaultState={{
-    //       center: [55.75, 37.57],
-    //       zoom: 9,
-    //     }}
-    //     width="100%"
-    //     height="40vh"
-    //   >
-    //     <Placemark
-    //       options={{
-    //         preset: "islands#circleDotIcon",
-    //         iconColor: "#ff0000",
-    //       }}
-    //       properties={{
-    //         iconCaption: true,
-    //       }}
-    //       geometry={[55.75, 37.57]}
-    //     />
-    //   </Map>
-    // </YMaps>
+    <div className="map">
+      <MapContainer
+        style={{ height: "450px", width: "100%" }}
+        center={center}
+        zoom={11}
+      >
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {marks.length
+          ? marks.map((item) => {
+              return (
+                <Marker
+                  position={[item.lat, item.lon]}
+                  key={item.place_id}
+                  icon={customMarker}
+                >
+                  <MyComponent />
+                  <Popup>{`${item.address.city} ${item.address.road}`}</Popup>
+                </Marker>
+              );
+            })
+          : ""}
+      </MapContainer>
+    </div>
   );
 };
 
