d3cc16cd2d7614b46bc8e77266a2df1f295833e2
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-08-03 21:27:34 +0300

    F-03 Изменение API нахождения точек адресов

--- src/actions/geolocate.js
+++ src/actions/geolocate.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { API_GEO, API_GEO_KEY } from "../reducers/data/dataServer";
+import { API_GEO } from "../reducers/data/dataServer";
 import { setPlaceMarks } from "../reducers/appReducer";
 import { store } from "../reducers";
 
@@ -7,13 +7,9 @@ const getLocale = (query) => {
   return async (dispatch) => {
     try {
       const response = await axios.get(
-        `${API_GEO}/?format=json&apikey=${API_GEO_KEY}&geocode=${query}`
+        `${API_GEO}?q=${query}&format=json&addressdetails=1&limit=1`
       );
-      dispatch(
-        setPlaceMarks(
-          response.data.response.GeoObjectCollection.featureMember[0].GeoObject
-        )
-      );
+      dispatch(setPlaceMarks(response.data[0]));
     } catch (e) {
       console.error(e.response);
     }
@@ -21,8 +17,8 @@ const getLocale = (query) => {
 };
 
 export const updateAddresses = (points) => {
-  return points.map((item) => {
-    return store.dispatch(getLocale(`${item.cityId.name} ${item.address}`));
+  return points.forEach((item) => {
+    store.dispatch(getLocale(`${item.cityId.name} ${item.address}`));
   });
 };
 
