e15ebcd915f6027b8a15b01cf0862ba17af2680b
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-08-07 14:00:37 +0300

    V-04 Файл ввода доп настроек заказа

---
+++ src/components/Order/Extra/InputExtraServices.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { additional } from "../../../reducers/data/dataOrder";
+
+const InputExtraServices = () => {
+  return (
+    <aside>
+      <h1>Доп услуги</h1>
+      <section className="extra-additional">
+        {additional.map((item) => {
+          return (
+            <label key={item} htmlFor={item}>
+              {item}
+              <input type="checkbox" name="additional" id={item} value={item} />
+              <span className="check-mark" />
+            </label>
+          );
+        })}
+      </section>
+    </aside>
+  );
+};
+
+export default InputExtraServices;
