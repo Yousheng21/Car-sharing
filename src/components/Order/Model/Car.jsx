27f16993e6d3e8bcef7827f2f63b7358ba83c4d6
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-08-08 20:22:55 +0300

    F-04 Создание файла вывода списка машин

---
+++ src/components/Order/Model/Car.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import classNames from "classnames";
+import { API_URL } from "../../../reducers/data/dataServer";
+
+const Car = ({ item, handleClick, inputCar }) => {
+  return (
+    <button
+      type="button"
+      className={classNames({
+        car: true,
+        active: item.name === inputCar,
+      })}
+      onClick={() => {
+        handleClick(item);
+      }}
+    >
+      <div>
+        <h1 className="car-title">{item.name}</h1>
+        <span className="car-subtitle">
+          {item.priceMin} - {item.priceMax} P
+        </span>
+      </div>
+      <img
+        crossOrigin="anonymous"
+        referrerPolicy="origin"
+        src={API_URL + item.thumbnail.path}
+        alt={item.name}
+      />
+    </button>
+  );
+};
+
+export default Car;
