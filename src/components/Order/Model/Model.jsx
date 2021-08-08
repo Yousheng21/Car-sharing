359c7af643d8cacca97f51d3cddaf1c9fa536a64
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-08-08 20:24:19 +0300

    F-04 Разделение на отд компоненты

--- src/components/Order/Model/Model.jsx
+++ src/components/Order/Model/Model.jsx
@@ -1,20 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import classNames from "classnames";
 import "./model.scss";
 
-import getTableCars, { selectSortCars } from "../../../actions/app";
+import getTableCars from "../../../actions/app";
 
-import { API_URL } from "../../../reducers/data/dataServer";
 import OrderLayout from "../../layouts/OrderLayout/OrderLayout";
 import { setCurrentCar } from "../../../reducers/appReducer";
 
-import { categories } from "../../../reducers/data/dataOrder";
+import LabelCategory from "./LabelCategory";
+import Car from "./Car";
 
 const Model = ({ nextStep, page }) => {
   const dispatch = useDispatch();
 
-  const [inputCategory, setInputCategory] = useState(0);
   const [inputCar, setInputCar] = useState("");
   const [inputCarValid, setInputCarValid] = useState(false);
 
@@ -29,11 +27,6 @@ const Model = ({ nextStep, page }) => {
       setInputCarValid(true);
     }
   }, [cars.length]);
-
-  function handleChange(value, sort) {
-    selectSortCars(sort);
-    setInputCategory(+value);
-  }
 
   function handleClick(item) {
     setInputCar(item.name === inputCar ? "" : item.name);
@@ -51,58 +44,18 @@ const Model = ({ nextStep, page }) => {
     >
       <main className="model-content">
         <section className="model-input">
-          {categories.map((item, index) => {
-            return (
-              <label
-                className={classNames({
-                  active: index === inputCategory,
-                })}
-                key={item.text}
-                htmlFor={index}
-              >
-                <input
-                  type="radio"
-                  defaultChecked={index === 0}
-                  name="category"
-                  id={index}
-                  value={item.sort}
-                  onChange={() => handleChange(index, item.sort)}
-                />
-                {item.text}
-              </label>
-            );
-          })}
+          <LabelCategory />
         </section>
         <section className="model-cars">
           {tableCars.length
-            ? tableCars.map((item) => {
-                return (
-                  <button
-                    type="button"
-                    key={item.id}
-                    className={classNames({
-                      car: true,
-                      active: item.name === inputCar,
-                    })}
-                    onClick={() => {
-                      handleClick(item);
-                    }}
-                  >
-                    <div>
-                      <h1 className="car-title">{item.name}</h1>
-                      <span className="car-subtitle">
-                        {item.priceMin} - {item.priceMax} P
-                      </span>
-                    </div>
-                    <img
-                      crossOrigin="anonymous"
-                      referrerPolicy="origin"
-                      src={API_URL + item.thumbnail.path}
-                      alt={item.name}
-                    />
-                  </button>
-                );
-              })
+            ? tableCars.map((item) => (
+                <Car
+                  key={item.id}
+                  handleClick={handleClick}
+                  inputCar={inputCar}
+                  item={item}
+                />
+              ))
             : "Машины данной категории отсутствуют"}
         </section>
       </main>
