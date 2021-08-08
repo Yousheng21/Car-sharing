48039134ef606c2542664ebb2d5129713316343b
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-08-08 19:11:34 +0300

    F-03 Создание файла-каркаса для инпутов

---
+++ src/components/Order/Location/InputWrapper.jsx
@@ -0,0 +1,68 @@
+import React, { useEffect } from "react";
+import classNames from "classnames";
+import Close from "../../../images/close.svg";
+import Tooltip from "../../../utils/Input/Tooltip";
+import Input from "../../../utils/Input/Input";
+
+const InputWrapper = ({
+  input,
+  array,
+  current,
+  auto,
+  title,
+  onChange,
+  print,
+  id,
+}) => {
+  useEffect(() => {
+    if (current) input.onClick(current);
+  }, [current]);
+
+  const classClose = classNames({
+    "btn-close": true,
+    open: input.focus && input.value,
+  });
+
+  const classAuto = classNames({
+    "auto-address": true,
+    open: input.focus && auto,
+  });
+  return (
+    <section className="city-content">
+      <span>{title}</span>
+      <aside onFocus={input.onFocus} onBlur={input.onBlur}>
+        <Input
+          name={id}
+          value={input.value}
+          onChange={onChange}
+          placeholder={id === "city" ? "" : "Начните ввводить пункт выдачи"}
+        />
+        <button type="button" onClick={input.onClose} className={classClose}>
+          <Close />
+        </button>
+        <div className={classAuto}>
+          {array.length &&
+            array.map((item) => (
+              <Tooltip
+                key={item.name ?? item.address.road}
+                value={
+                  item.name ??
+                  `${item.address.road} ${item.address.house_number ?? ""}`
+                }
+                click={onChange}
+                text={
+                  item.name ??
+                  `${item.address.city} ${item.address.road} ${
+                    item.address.house_number ?? ""
+                  }`
+                }
+              />
+            ))}
+        </div>
+      </aside>
+      <span>{input.isDirty && input.printError(print)}</span>
+    </section>
+  );
+};
+
+export default InputWrapper;
