b689b9c313b5250f4671273eb5265bb450bb4784
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-08-08 21:07:55 +0300

    F-04 Реализация получения массива категорий с сервера

--- src/components/Order/Model/LabelCategory.jsx
+++ src/components/Order/Model/LabelCategory.jsx
@@ -1,10 +1,18 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import classNames from "classnames";
+import { useDispatch, useSelector } from "react-redux";
 import { selectSortCars } from "../../../actions/app";
-import { categories } from "../../../reducers/data/dataOrder";
+import getCategories from "../../../actions/category";
 
 const LabelCategory = () => {
+  const dispatch = useDispatch();
   const [inputCategory, setInputCategory] = useState(0);
+
+  const categories = useSelector((state) => state.app.categories);
+
+  useEffect(() => {
+    if (!categories.length) dispatch(getCategories);
+  }, [categories.length]);
 
   function handleChange(value, sort) {
     selectSortCars(sort);
@@ -15,7 +23,7 @@ const LabelCategory = () => {
       className={classNames({
         active: index === inputCategory,
       })}
-      key={item.text}
+      key={item.id}
       htmlFor={index}
     >
       <input
@@ -23,10 +31,10 @@ const LabelCategory = () => {
         defaultChecked={index === 0}
         name="category"
         id={index}
-        value={item.sort}
-        onChange={() => handleChange(index, item.sort)}
+        value={item.name}
+        onChange={() => handleChange(index, item.id)}
       />
-      {item.text}
+      {item.name}
     </label>
   ));
 };
