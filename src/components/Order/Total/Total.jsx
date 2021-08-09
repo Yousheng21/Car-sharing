32090744c6ca95323ef0a3447a6711b602fdb849
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-08-09 21:26:54 +0300

    V-05 Переход к нач состоянию

--- src/components/Order/Total/Total.jsx
+++ src/components/Order/Total/Total.jsx
@@ -1,14 +1,28 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import "./total.scss";
 
+import { API_URL } from "../../../reducers/data/dataServer";
 import OrderLayout from "../../layouts/OrderLayout/OrderLayout";
-import InfoOrderLayout from "../../layouts/InfoOrderLayout";
 
 const Total = ({ nextStep, page }) => {
+  const car = useSelector((state) => state.app.currentCar);
+  const order = useSelector((state) => state.app.currentOrder);
+
   return (
     <OrderLayout path="confirm" text="Заказать" page={page} step={nextStep}>
       <main className="total-content">
-        <InfoOrderLayout />
+        <section className="total-content-info">
+          <h1>{car.name}</h1>
+          <span className="number">{car.number}</span>
+          <section className="text">
+            <span>Топливо</span> {order.cistern.value}
+          </section>
+          <section className="text">
+            <span>Доступна c</span> {order.delay.from}
+          </section>
+        </section>
+        <img src={API_URL + car.thumbnail.path} alt={car.name} />
       </main>
     </OrderLayout>
   );
