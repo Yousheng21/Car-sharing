78934e81e4da383a51a3891fbf495a8fb5edc15a
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-08-11 18:43:39 +0300

    f-05 Получение и запись тарифы в state

--- src/components/Order/Extra/inputs/InputTariff.jsx
+++ src/components/Order/Extra/inputs/InputTariff.jsx
@@ -1,29 +1,44 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import classNames from "classnames";
-import { tariff } from "../../../../reducers/data/dataOrder";
+import { useDispatch, useSelector } from "react-redux";
+import getTariffs from "../../../../actions/tariff";
+import { setTariff } from "../../../../reducers/appReducer";
 
 const InputTariff = () => {
+  const dispatch = useDispatch();
   const [inputTariff, setInputTariff] = useState(0);
+  const tariffs = useSelector((state) => state.app.tariffs);
+
+  useEffect(() => {
+    if (!tariffs.length) dispatch(getTariffs);
+    else dispatch(setTariff(tariffs[0].rateTypeId.name));
+  }, [tariffs.length]);
+
+  const handleChange = (index, name) => {
+    setInputTariff(index);
+    dispatch(setTariff(name));
+  };
+
   return (
     <div>
       <h1>Тариф</h1>
       <section className="extra-tariff">
-        {tariff.map((item, index) => (
+        {tariffs.map((item, index) => (
           <label
             className={classNames({
               active: index === inputTariff,
             })}
-            key={item}
-            htmlFor={item}
+            key={item.id}
+            htmlFor={item.rateTypeId.id}
           >
             <input
               type="radio"
               name="tariff"
-              id={item}
-              value={item}
-              onChange={() => setInputTariff(index)}
+              id={item.rateTypeId.id}
+              value={item.rateTypeId.id}
+              onChange={() => handleChange(index, item.rateTypeId.name)}
             />
-            {item}
+            {`${item.rateTypeId.name}, ${item.price} P/${item.rateTypeId.unit}`}
           </label>
         ))}
       </section>
