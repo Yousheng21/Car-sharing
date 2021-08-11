158bbcd3ac8bf9fd85619927b89c2fdc2f392f9a
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-08-11 18:47:53 +0300

    F-05 Добавление доп параметрам марки цены

--- src/reducers/data/dataOrder.js
+++ src/reducers/data/dataOrder.js
@@ -2,9 +2,9 @@ export const center = [54.3282, 48.3866];
 export const tariff = ["Поминутно,7 р/мин", "На сутки, 1999 р/сутки"];
 
 export const additional = [
-  "Полный бак, 500р",
-  "Детское кресло, 200р",
-  "Правый руль, 1600р",
+  { name: "Полный бак", price: 500 },
+  { name: "Детское кресло", price: 200 },
+  { name: "Правый руль", price: 1600 },
 ];
 
 export const categoryAll = { name: "Все модели", id: "" };
