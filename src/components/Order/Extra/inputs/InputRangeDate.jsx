80234a1713f07c782cc0065cb07247094375e078
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-08-11 18:43:22 +0300

    f-05 вывод даты

--- src/components/Order/Extra/inputs/InputRangeDate.jsx
+++ src/components/Order/Extra/inputs/InputRangeDate.jsx
@@ -3,7 +3,10 @@ import classNames from "classnames";
 import Close from "../../../../images/close.svg";
 
 const InputRangeDate = () => {
-  const [inputDateFrom, setInputDateFrom] = useState("");
+  const date = new Date();
+  const [inputDateFrom, setInputDateFrom] = useState(
+    `${date.toJSON().split("T")[0]}T${date.toLocaleTimeString()}`
+  );
   const [inputDateTo, setInputDateTo] = useState("");
   const [inputDateFocusTo, setInputDateFocusTo] = useState(false);
   const [inputDateFocusFrom, setInputDateFocusFrom] = useState(false);
@@ -17,7 +20,7 @@ const InputRangeDate = () => {
     "btn-close": true,
     open: inputDateFocusFrom,
   });
-
+  console.log(inputDateFrom);
   return (
     <div>
       <h1>Дата аренды</h1>
@@ -26,13 +29,15 @@ const InputRangeDate = () => {
           <span>С</span>
           <div>
             <input
-              type="date"
+              type="datetime-local"
               value={inputDateFrom}
               onChange={(event) => setInputDateFrom(event.target.value)}
               onFocus={() => setInputDateFocusFrom(true)}
               onBlur={() => setInputDateFocusFrom(false)}
               name="dateFrom"
               id="dateFrom"
+              min={inputDateFrom}
+              pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}"
             />
             <Close
               onClick={() => {
@@ -46,14 +51,17 @@ const InputRangeDate = () => {
           <span>По</span>
           <div>
             <input
-              type="date"
+              type="datetime-local"
               name="dateTo"
               onChange={(event) => setInputDateTo(event.target.value)}
               onFocus={() => setInputDateFocusTo(true)}
               onBlur={() => setInputDateFocusTo(false)}
               placeholder="Введите дату и время"
               id="dateTo"
+              min={inputDateFrom}
               value={inputDateTo}
+              disabled={!inputDateFrom}
+              pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}"
             />
             <Close
               onClick={() => {
