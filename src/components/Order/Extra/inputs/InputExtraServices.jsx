3cb2550d76ff83ac6f2a29a31648fb1a1690d500
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-08-11 18:42:36 +0300

    f-05 Получение и запись доп параметров машины в state

--- src/components/Order/Extra/inputs/InputExtraServices.jsx
+++ src/components/Order/Extra/inputs/InputExtraServices.jsx
@@ -1,15 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { additional } from "../../../../reducers/data/dataOrder";
+import {
+  changeAdditional,
+  setAdditional,
+} from "../../../../reducers/appReducer";
 
 const InputExtraServices = () => {
+  const dispatch = useDispatch();
+  const [arrSelected, setArrSelected] = useState([]);
+
+  const additionalState = useSelector(
+    (state) => state.app.currentOrder.additional
+  );
+
+  const handleChange = (item, index) => {
+    if (!arrSelected.includes(index)) {
+      dispatch(setAdditional(item));
+      setArrSelected([...arrSelected, index]);
+    } else {
+      const elIndex = additionalState.findIndex((el) => {
+        return el.text === item.name;
+      });
+      dispatch(changeAdditional(elIndex));
+      arrSelected.splice(elIndex, 1);
+      setArrSelected([...arrSelected]);
+    }
+  };
+
   return (
     <div>
       <h1>Доп услуги</h1>
       <section className="extra-additional">
-        {additional.map((item) => (
-          <label key={item} htmlFor={item}>
-            {item}
-            <input type="checkbox" name="additional" id={item} value={item} />
+        {additional.map((item, index) => (
+          <label key={item.name} htmlFor={item.name}>
+            {`${item.name}, ${item.price}P`}
+            <input
+              type="checkbox"
+              onChange={() => handleChange(item, index)}
+              name="additional"
+              id={item.name}
+              value={item.name}
+            />
             <span className="check-mark" />
           </label>
         ))}
