10cabbdaabec05ae10e15acb34fb567c13193281
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-08-12 22:25:53 +0300

    F-05 Передача состояний пропсов для отслеживания дизейбла основной кнопки

--- src/components/Order/Extra/Extra.jsx
+++ src/components/Order/Extra/Extra.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import "./extra.scss";
 
+import { useSelector } from "react-redux";
 import OrderLayout from "../../layouts/OrderLayout/OrderLayout";
 import InputColor from "./inputs/InputColor";
 import InputRangeDate from "./inputs/InputRangeDate";
@@ -8,11 +9,37 @@ import InputTariff from "./inputs/InputTariff";
 import InputExtraServices from "./inputs/InputExtraServices";
 
 const Extra = ({ nextStep, page }) => {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(
+    startDate
+      ? new Date(
+          startDate.getFullYear(),
+          startDate.getMonth(),
+          startDate.getDate(),
+          startDate.getHours(),
+          startDate.getMinutes() + 30
+        )
+      : ""
+  );
+
+  const dateIsValid = useSelector((state) => state.app.dateIsValid);
   return (
-    <OrderLayout path="total" step={nextStep} page={page} text="Итого">
+    <OrderLayout
+      path="total"
+      step={nextStep}
+      page={page}
+      text="Итого"
+      arrayValid={[dateIsValid, startDate, endDate]}
+    >
       <main className="extra-content">
         <InputColor />
-        <InputRangeDate />
+        <InputRangeDate
+          dateIsValid={dateIsValid}
+          startDate={startDate}
+          setStartDate={setStartDate}
+          endDate={endDate}
+          setEndDate={setEndDate}
+        />
         <InputTariff />
         <InputExtraServices />
       </main>
