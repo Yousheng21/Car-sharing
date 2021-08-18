6e0dcdfbaafee99e482f2629a7dc38cff8a47f66
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-08-18 03:06:06 +0300

    F-04 добавление ключа в бд параметрам машины

--- src/reducers/data/dataOrder.js
+++ src/reducers/data/dataOrder.js
@@ -2,9 +2,9 @@ export const center = [54.3282, 48.3866];
 export const tariff = ["Поминутно,7 р/мин", "На сутки, 1999 р/сутки"];
 
 export const additional = [
-  { name: "Полный бак", price: 500 },
-  { name: "Детское кресло", price: 200 },
-  { name: "Правый руль", price: 1600 },
+  { name: "Полный бак", price: 500, key: "isFullTank" },
+  { name: "Детское кресло", price: 200, key: "isNeedChildChair" },
+  { name: "Правый руль", price: 1600, key: "isRightWheel" },
 ];
 
 export const categoryAll = { name: "Все модели", id: "" };
