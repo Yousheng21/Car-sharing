e465a07b49f5d090fe241dc11ae3ef35257e54f0
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-08-18 03:05:25 +0300

    V-05 Изменение рендера итогового файла с информацией о заказе

--- src/components/Order/Total/Total.jsx
+++ src/components/Order/Total/Total.jsx
@@ -1,16 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import "./total.scss";
 
-import OrderLayout from "../../layouts/OrderLayout/OrderLayout";
+import { Link } from "react-router-dom";
 import InfoOrderLayout from "../../layouts/InfoOrderLayout";
+import BurgerMenu from "../../common/Burger-menu/Burger-menu";
+import NavBar from "../../common/NavBar/NavBar";
+import SideBar from "../../common/SideBar/SideBar";
+import Modal from "../../../utils/Modal/Modal";
+import { setOrder } from "../../../actions/order";
 
-const Total = ({ nextStep, page }) => {
+const Total = ({ page }) => {
+  const [activeModal, setActiveModal] = useState(false);
+
   return (
-    <OrderLayout path="confirm" text="Заказать" page={page} step={nextStep}>
+    <div className="location-page">
+      <BurgerMenu />
+      <NavBar page={page} />
       <main className="total-content">
         <InfoOrderLayout />
       </main>
-    </OrderLayout>
+      <SideBar>
+        <button
+          type="button"
+          onClick={() => {
+            setActiveModal(true);
+          }}
+          className="button sideBar-button sideBar-child"
+        >
+          Заказать
+        </button>
+      </SideBar>
+      <Modal active={activeModal} total>
+        <h1>Подтвердить заказ</h1>
+        <Link className="button" to="/car-sharing/order/confirm">
+          <button type="button" className="success" onClick={setOrder}>
+            Подтвердить
+          </button>
+        </Link>
+        <button
+          type="button"
+          className="canceled"
+          onClick={() => setActiveModal(false)}
+        >
+          Вернуться
+        </button>
+      </Modal>
+    </div>
   );
 };
 
