dc4971b44ec3f00ea1fc907e25febc88cf6704ed
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-08-18 03:02:36 +0300

    F-06 файл сохранения заказа

---
+++ src/actions/order.js
@@ -0,0 +1,50 @@
+import { instance } from "../reducers/data/dataServer";
+import { store } from "../reducers";
+
+const setOrderTable = (req) => {
+  return async () => {
+    try {
+      const response = await instance({
+        method: "POST",
+        url: "/api/db/order",
+        data: req,
+      });
+      console.log(response);
+    } catch (e) {
+      console.error(e.response);
+    }
+  };
+};
+
+export const setOrder = () => {
+  const city = store.getState().app.currentCity;
+  const point = store.getState().app.currentAddress;
+  const car = store.getState().app.currentCar;
+  const color = store.getState().app.currentOrder.color.value;
+  const dateFrom = store.getState().app.currentOrder.delay.from;
+  const dateTo = store.getState().app.currentOrder.delay.to;
+  const rate = store.getState().app.currentTariff;
+  const price = store.getState().app.price.value;
+  const { additional } = store.getState().app.currentOrder;
+
+  const result = {
+    orderStatusId: {
+      id: "5e26a1f0099b810b946c5d8b",
+    },
+    cityId: city,
+    pointId: point,
+    carId: car,
+    color,
+    dateFrom,
+    dateTo,
+    rateId: rate,
+    price,
+    isFullTank: additional.isFullTank.value,
+    isNeedChildChair: additional.isNeedChildChair.value,
+    isRightWheel: additional.isRightWheel.value,
+  };
+
+  store.dispatch(setOrderTable(result));
+};
+
+export default setOrderTable();
