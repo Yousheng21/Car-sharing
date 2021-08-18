8031bb84bf8480072dcc0cd9d20ece7cb152ea99
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-08-18 03:02:58 +0300

    V Изменение вывода доп параметров машины

--- src/components/common/SideBar/Additional.jsx
+++ src/components/common/SideBar/Additional.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 
 const Additional = ({ arr }) => {
-  return arr.length
-    ? arr.map((item) => (
-        <section key={item.text} className="sideBar-info-content">
-          <span className="place">{item.text}</span>
-          <span className="dots">......................</span>
-          <div className="address">
-            <span>{item.value}</span>
-          </div>
-        </section>
-      ))
-    : "";
+  return Object.keys(arr).map((item) =>
+    arr[item].value ? (
+      <section key={arr[item].text} className="sideBar-info-content">
+        <span className="place">{arr[item].text}</span>
+        <span className="dots">......................</span>
+        <div className="address">
+          <span>Да</span>
+        </div>
+      </section>
+    ) : (
+      ""
+    )
+  );
 };
 
 export default Additional;
