38f1bb642db073327f5a25665186c01ebab79541
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-08-19 04:57:58 +0300

    V-06 Логика вывода размера топлива машины

--- src/components/layouts/InfoOrderLayout.jsx
+++ src/components/layouts/InfoOrderLayout.jsx
@@ -11,10 +11,13 @@ const InfoOrderLayout = () => {
         <h1>{car.name}</h1>
         {car.number ? <span className="number">{car.number}</span> : ""}
         <section className="text">
-          <span>Топливо</span>
+          <span>
+            Топливо
+            {order.additional.isFullTank.value ? " 100" : car.tank ?? " 0"}%
+          </span>
         </section>
         <section className="text">
-          <span>Доступна c</span> {order.delay.from}
+          <span>Доступна c</span> {order.delay.from.toLocaleString()}
         </section>
       </section>
       <img src={API_URL + car.thumbnail.path} alt={car.name} />
