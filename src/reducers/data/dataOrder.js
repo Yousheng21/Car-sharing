b79333ee47b48b8eceb926504c49057caede2f0a
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-08-19 05:00:12 +0300

    F-06 Добавление пер нового заказа перед подтверждением

--- src/reducers/data/dataOrder.js
+++ src/reducers/data/dataOrder.js
@@ -1,5 +1,6 @@
 export const center = [54.3282, 48.3866];
 export const orderCompleteId = "5e26a1f0099b810b946c5d8b";
+export const orderNewId = "5e26a191099b810b946c5d89";
 
 export const additional = [
   { name: "Полный бак", price: 500, key: "isFullTank" },
