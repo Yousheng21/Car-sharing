38c05f491e09c009efeb5d29fad464af17b9337e
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-08-19 04:58:47 +0300

    V-06 Добавление записи id заказа при клике подтвердить

--- src/components/Order/Total/Total.jsx
+++ src/components/Order/Total/Total.jsx
@@ -1,16 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./total.scss";
 
 import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
 import InfoOrderLayout from "../../layouts/InfoOrderLayout";
 import BurgerMenu from "../../common/Burger-menu/Burger-menu";
 import NavBar from "../../common/NavBar/NavBar";
 import SideBar from "../../common/SideBar/SideBar";
 import Modal from "../../../utils/Modal/Modal";
 import { setOrder } from "../../../actions/order";
+import setOrderComplete from "../../../actions/confirmOrder";
 
 const Total = ({ page }) => {
+  const dispatch = useDispatch();
   const [activeModal, setActiveModal] = useState(false);
+  const orderId = useSelector((state) => state.app.orderId);
+  const order = useSelector((state) => state.app.currentOrder);
+
+  useEffect(() => {
+    dispatch(setOrder);
+  }, [order]);
 
   return (
     <div className="location-page">
@@ -32,8 +41,12 @@ const Total = ({ page }) => {
       </SideBar>
       <Modal active={activeModal} total>
         <h1>Подтвердить заказ</h1>
-        <Link className="button" to="/car-sharing/order/confirm">
-          <button type="button" className="success" onClick={setOrder}>
+        <Link className="button" to={`/car-sharing/order/confirm/${orderId}`}>
+          <button
+            type="button"
+            className="success"
+            onClick={() => dispatch(setOrderComplete(orderId))}
+          >
             Подтвердить
           </button>
         </Link>
