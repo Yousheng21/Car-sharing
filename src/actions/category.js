011c5ca59cf9f3e14cca1011a6526110ae946109
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-08-20 04:10:42 +0300

    F-04 Вывод всех категорий машин

--- src/actions/category.js
+++ src/actions/category.js
@@ -2,17 +2,12 @@ import { instance } from "../reducers/data/dataServer";
 import { setCategories } from "../reducers/appReducer";
 import { categoryAll } from "../reducers/data/dataOrder";
 
-const LIMIT_VALUE = 3;
-
 const getCategories = () => {
   return async (dispatch) => {
     try {
       const response = await instance({
         method: "GET",
         url: "/api/db/category",
-        params: {
-          limit: LIMIT_VALUE,
-        },
       });
       response.data.data.unshift(categoryAll);
       dispatch(setCategories(response.data.data));
