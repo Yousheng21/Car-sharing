bb13c7bcb93cf2ce00ff8438371fb163468381b1
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-08-21 00:09:44 +0300

    F-05 доб id статуса отменненого заказа

--- src/reducers/data/dataOrder.js
+++ src/reducers/data/dataOrder.js
@@ -1,6 +1,7 @@
 export const center = [54.3282, 48.3866];
 export const orderCompleteId = "5e26a1f0099b810b946c5d8b";
 export const orderNewId = "5e26a191099b810b946c5d89";
+export const orderCloseId = "5e26a1f5099b810b946c5d8c";
 
 export const additional = [
   { name: "Полный бак", price: 500, key: "isFullTank" },
